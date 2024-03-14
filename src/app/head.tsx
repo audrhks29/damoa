d6218b4d96e042249ef64f79fc86f349c95decaa
audrhks29 <audrhks29@gmail.com>
2024-03-14 18:04:38 +0900

    Update v1.0.4

---
+++ src/app/head.tsx
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      {/* 다른 메타 태그나 head 컨텐츠 */}
+    </>
+  )
+}
