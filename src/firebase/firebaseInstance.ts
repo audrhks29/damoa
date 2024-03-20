f4f393afd530ee01f86b58f21ea218576179697d
audrhks29 <audrhks29@gmail.com>
2024-03-21 03:32:30 +0900

    Update v1.3.0

--- src/firebase/firebaseInstance.ts
+++ src/firebase/firebaseInstance.ts
@@ -7,13 +7,13 @@ import { getAuth } from 'firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCv7MLFBwTnx6imjmdP-CBDtACWPy4KDMQ",
-  authDomain: "damoa-c03cf.firebaseapp.com",
-  projectId: "damoa-c03cf",
-  storageBucket: "damoa-c03cf.appspot.com",
-  messagingSenderId: "18847619988",
-  appId: "1:18847619988:web:ff283ef8be89f84c050d81",
-  measurementId: "G-PN1BGX09L2"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
